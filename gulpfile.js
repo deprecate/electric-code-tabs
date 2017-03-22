b5d45053b4c2bd5ba0681abcc9ce8526f171cb73
Fernando Souza <fernando.souza@liferay.com>
2017-03-22 16:18:22 -0300

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var metal = require('gulp-metal');
+
+metal.registerTasks({
+	bundleCssFileName: 'electric-code-mirror.css',
+	bundleFileName: 'electric-code-mirror.js',
+	moduleName: 'metal-electric-code-mirror'
+});
---
+++ test/ElectricCodeMirror.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import ElectricCodeMirror from '../src/ElectricCodeMirror';
+
+describe('ElectricCodeMirror', function() {
+	it('should be tested', function() {
+		assert.fail('No tests for this module yet.');
+	});
+});
